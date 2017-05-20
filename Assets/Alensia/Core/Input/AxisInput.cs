2bd167337532a899037d8a57a48c3c3df35cd973
Xavier Cho <mysticfallband@gmail.com>
2017-05-20 16:41:24 +0900

    Add smoothing support for AxisInput

--- Assets/Alensia/Core/Input/AxisInput.cs
+++ Assets/Alensia/Core/Input/AxisInput.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UniRx;
+using UnityEngine;
 using UnityEngine.Assertions;
 using static UnityEngine.Input;
 
@@ -10,19 +11,36 @@ namespace Alensia.Core.Input
     {
         public string Axis { get; }
 
-        public AxisInput(string axis) : this(axis, Enumerable.Empty<ITrigger>().ToList())
+        public float? Smoothing { get; }
+
+        public AxisInput(string axis) :
+            this(axis, null, Enumerable.Empty<ITrigger>().ToList())
         {
         }
 
-        public AxisInput(string axis, IList<ITrigger> modifiers) : base(modifiers)
+        public AxisInput(string axis, float? smoothing) :
+            this(axis, smoothing, Enumerable.Empty<ITrigger>().ToList())
+        {
+        }
+
+        public AxisInput(string axis, float? smoothing, IList<ITrigger> modifiers) : base(modifiers)
         {
             Assert.IsNotNull(axis, "axis != null");
 
             Axis = axis;
+            Smoothing = smoothing;
         }
 
         protected override IObservable<float> Observe(IObservable<long> onTick)
         {
+            if (Smoothing.HasValue)
+            {
+                return onTick
+                    .Select(_ => GetAxis(Axis))
+                    .Scan((previous, current) =>
+                        Mathf.Lerp(previous, current, Time.deltaTime / Smoothing.Value));
+            }
+
             return onTick.Select(_ => GetAxis(Axis));
         }
     }
