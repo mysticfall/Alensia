8bcd62713017e7565b727d8bb44ea159ad3b3265
Xavier Cho <mysticfallband@gmail.com>
2017-05-28 16:52:58 +0900

    Add basic UMA integration support

---
+++ Assets/Alensia/Integrations/UMA/AlensiaIntegrationSlot.cs
@@ -0,0 +1,30 @@
+using UMA;
+using UnityEngine;
+using Zenject;
+
+namespace Alensia.Integrations.UMA
+{
+    public class AlensiaIntegrationSlot : MonoBehaviour
+    {
+        public void OnCharacterBegun(UMAData umaData)
+        {
+        }
+
+        public void OnSlotAtlassed(
+            UMAData umaData, SlotData slotData, Material material, Rect region)
+        {
+        }
+
+        public void OnDnaApplied(UMAData umaData)
+        {
+        }
+
+        public void OnCharacterCompleted(UMAData umaData)
+        {
+            var character = umaData.transform.root;
+            var context = character.GetComponentInChildren<GameObjectContext>();
+
+            context?.Run();
+        }
+    }
+}
