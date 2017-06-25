bc66c90ba83f4937636a1e0bea73d9455e58ae9a
Xavier Cho <mysticfallband@gmail.com>
2017-06-25 12:12:20 +0900

    Add IsDestroyed() extension method to IManagedEntity

--- Assets/Alensia/Core/Common/IManagedEntity.cs
+++ Assets/Alensia/Core/Common/IManagedEntity.cs
@@ -6,4 +6,16 @@ namespace Alensia.Core.Common
     {
         GameObject GameObject { get; }
     }
+
+    public static class ManagedEntityExtensions
+    {
+        public static bool IsDestroyed(this IManagedEntity entity) => IsDestroyed(entity.GameObject);
+
+        public static bool IsDestroyed(this GameObject gameObject)
+        {
+            // This works because Unity overrides '==' for GameObject.
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+            return gameObject == null && !ReferenceEquals(gameObject, null);
+        }
+    }
 }
