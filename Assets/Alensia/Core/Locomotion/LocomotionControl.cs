2dee9fe7753c8873f111e479baf100d0f6811e69
Xavier Cho <mysticfallband@gmail.com>
2017-07-02 14:04:11 +0900

    Allow overriding of locomotion implementation

--- Assets/Alensia/Core/Locomotion/LocomotionControl.cs
+++ Assets/Alensia/Core/Locomotion/LocomotionControl.cs
@@ -1,20 +1,16 @@
 ﻿using Alensia.Core.Input;
-using UnityEngine.Assertions;
 
 namespace Alensia.Core.Locomotion
 {
     public abstract class LocomotionControl<T> : Control.Control, ILocomotionControl<T>
         where T : class, ILocomotion
     {
-        public T Locomotion { get; }
+        public abstract T Locomotion { get; }
 
-        public override bool Valid => base.Valid && Locomotion.Active;
+        public override bool Valid => base.Valid && Locomotion != null && Locomotion.Active;
 
-        protected LocomotionControl(T locomotion, IInputManager inputManager) : base(inputManager)
+        protected LocomotionControl(IInputManager inputManager) : base(inputManager)
         {
-            Assert.IsNotNull(locomotion, "locomotion != null");
-
-            Locomotion = locomotion;
         }
     }
 }
