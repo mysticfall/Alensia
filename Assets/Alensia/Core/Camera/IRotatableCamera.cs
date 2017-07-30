37f6615f7d9142107bb9699fde72e839d6e67e42
Xavier Cho <mysticfallband@gmail.com>
2017-07-30 12:36:55 +0900

    Add IDirectional

--- Assets/Alensia/Core/Camera/IRotatableCamera.cs
+++ Assets/Alensia/Core/Camera/IRotatableCamera.cs
@@ -1,19 +1,13 @@
-﻿using UnityEngine;
+﻿using Alensia.Core.Geom;
 
 namespace Alensia.Core.Camera
 {
-    public interface IRotatableCamera : ICameraMode
+    public interface IRotatableCamera : ICameraMode, IDirectional
     {
         RotationalConstraints RotationalConstraints { get; }
 
         float Heading { get; set; }
 
         float Elevation { get; set; }
-
-        Vector3 Pivot { get; }
-
-        Vector3 AxisForward { get; }
-
-        Vector3 AxisUp { get; }
     }
 }
---
+++ Assets/Alensia/Core/Geom/IDirectional.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Alensia.Core.Geom
+{
+    public interface IDirectional
+    {
+        Vector3 Pivot { get; }
+
+        Vector3 AxisForward { get; }
+
+        Vector3 AxisUp { get; }
+
+        Vector3 AxisRight { get; }
+    }
+}
