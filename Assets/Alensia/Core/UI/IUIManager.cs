429fb6fcd1a31dab82d047a130e26073a5eda4d8
Xavier Cho <mysticfallband@gmail.com>
2017-08-19 18:40:51 +0900

    Simplify IUIManager hierarchy

--- Assets/Alensia/Core/UI/IUIManager.cs
+++ Assets/Alensia/Core/UI/IUIManager.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
 using Alensia.Core.UI.Cursor;
 using Alensia.Core.UI.Screen;
+using UnityEngine;
 
 namespace Alensia.Core.UI
 {
-    public interface IUIManager : IUIContextHolder, IScreenManager, ICursorManager
+    public interface IUIManager : IUIContextHolder
     {
+        IReadOnlyList<IScreen> Screens { get; }
+
+        Transform ScreenRoot { get; }
+
+        CursorState CursorState { get; set; }
+
+        ICursorSet CursorSet { get; }
+
+        string DefaultCursor { get; set; }
+
+        IScreen FindScreen(string name);
+
+        void ShowScreen(string name);
+
+        void HideScreen(string name);
     }
 }
