6b81578e4f56fd80d5863cb7c247ceddc54fdf7c
Xavier Cho <mysticfallband@gmail.com>
2017-08-27 06:59:48 +0900

    Add event listener for mouse up/down

---
+++ Assets/Alensia/Core/UI/Event/IPointerSelectionAware.cs
@@ -0,0 +1,14 @@
+using UniRx;
+using UnityEngine.EventSystems;
+
+namespace Alensia.Core.UI.Event
+{
+    public interface IPointerSelectionAware
+    {
+        IObservable<PointerEventData> OnPointerPress { get; }
+
+        IObservable<PointerEventData> OnPointerRelease { get; }
+
+        IObservable<PointerEventData> OnPointerSelect { get; }
+    }
+}
