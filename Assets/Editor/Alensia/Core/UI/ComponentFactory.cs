c9accfd67018e19f1b502f1ab98923e16e1f1dfb
Xavier Cho <mysticfallband@gmail.com>
2017-09-04 11:41:19 +0900

    Rearrange UI menu according to component types

--- Assets/Editor/Alensia/Core/UI/ComponentFactory.cs
+++ Assets/Editor/Alensia/Core/UI/ComponentFactory.cs
@@ -6,20 +6,20 @@ namespace Alensia.Core.UI
 {
     public static class ComponentFactory
     {
+        [MenuItem("GameObject/UI/Alensia/Label", false, 10)]
+        public static Label CreateLabel(MenuCommand command) => CreateComponent(command, Label.CreateInstance);
+
         [MenuItem("GameObject/UI/Alensia/Button", false, 10)]
         public static Button CreateButton(MenuCommand command) => CreateComponent(command, Button.CreateInstance);
 
         [MenuItem("GameObject/UI/Alensia/Dropdown", false, 10)]
         public static Dropdown CreateDropdown(MenuCommand command) => CreateComponent(command, Dropdown.CreateInstance);
 
-        [MenuItem("GameObject/UI/Alensia/Label", false, 10)]
-        public static Label CreateLabel(MenuCommand command) => CreateComponent(command, Label.CreateInstance);
+        [MenuItem("GameObject/UI/Alensia/Slider", false, 10)]
+        public static Slider CreateSlider(MenuCommand command) => CreateComponent(command, Slider.CreateInstance);
 
         [MenuItem("GameObject/UI/Alensia/Panel", false, 10)]
         public static Panel CreatePanel(MenuCommand command) => CreateComponent(command, Panel.CreateInstance);
-
-        [MenuItem("GameObject/UI/Alensia/Slider", false, 10)]
-        public static Slider CreateSlider(MenuCommand command) => CreateComponent(command, Slider.CreateInstance);
 
         [MenuItem("GameObject/UI/Alensia/Scroll Panel", false, 10)]
         public static ScrollPanel CreateScrollPanel(MenuCommand command) => CreateComponent(command, ScrollPanel.CreateInstance);
