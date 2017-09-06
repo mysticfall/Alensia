539512044f61f20997110f387fde6433a8d8ab7b
Xavier Cho <mysticfallband@gmail.com>
2017-09-06 09:08:48 +0900

    Add PointerDragTracker

---
+++ Assets/Alensia/Core/UI/Event/PointerDragTracker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.EventSystems;
+
+namespace Alensia.Core.UI.Event
+{
+    public class PointerDragTracker<T> : EventTracker<T> where T : UIBehaviour
+    {
+        private IDisposable _listener;
+
+        public PointerDragTracker(T component) : base(component)
+        {
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+
+            _listener = Component.OnBeginDragAsObservable()
+                .Select(_ => true)
+                .Merge(Component.OnEndDragAsObservable().Select(_ => false))
+                .DistinctUntilChanged()
+                .Subscribe(ChangeState);
+        }
+
+        protected override void OnDeactivated()
+        {
+            _listener?.Dispose();
+            _listener = null;
+
+            base.OnDeactivated();
+        }
+    }
+}
