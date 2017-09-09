ecaaedda462a39000da51a5e9e021097739540e8
Xavier Cho <mysticfallband@gmail.com>
2017-09-09 12:22:49 +0900

    Add IPointerDragAware

---
+++ Assets/Alensia/Core/UI/Event/IPointerDragAware.cs
@@ -0,0 +1,14 @@
+﻿using UniRx;
+using UnityEngine.EventSystems;
+
+namespace Alensia.Core.UI.Event
+{
+    public interface IPointerDragAware
+    {
+        IObservable<PointerEventData> OnDragBegin { get; }
+
+        IObservable<PointerEventData> OnDrag { get; }
+
+        IObservable<PointerEventData> OnDragEnd { get; }
+    }
+}
