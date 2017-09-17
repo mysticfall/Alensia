692e5cbdf2fb3f36678a563ae13dcc8e26a4d1f2
Xavier Cho <mysticfallband@gmail.com>
2017-09-17 11:58:53 +0900

    Add extension method to get or add component to game object

---
+++ Assets/Alensia/Core/Common/ComponentExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Alensia.Core.Common
+{
+    public static class ComponentExtensions
+    {
+        public static T GetOrAddComponent<T>(this Component component) where T : Component
+        {
+            return component.GetComponent<T>() ?? component.gameObject.AddComponent<T>();
+        }
+    }
+}
