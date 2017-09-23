56681ca52a00b1cf7796a9ac3ef1e37186cc118f
Xavier Cho <mysticfallband@gmail.com>
2017-09-23 19:56:37 +0900

    Add event tracker to track input focus

---
+++ Assets/Alensia/Core/UI/Event/FocusTracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UniRx;
+using UnityEngine.UI;
+
+namespace Alensia.Core.UI.Event
+{
+    public class FocusTracker<T> : EventTracker<T> where T : InputField
+    {
+        private IDisposable _listener;
+
+        public FocusTracker(T component) : base(component)
+        {
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+
+            _listener = Component
+                .ObserveEveryValueChanged(c => c.isFocused)
+                .Subscribe(ChangeState);
+        }
+
+        protected override void OnDeactivated()
+        {
+            _listener?.Dispose();
+            _listener = null;
+
+            base.OnDeactivated();
+        }
+    }
+}
