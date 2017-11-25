86af6ca2379b8e1e7ebcdcfad476ab8a5d7a8a46
Xavier Cho <mysticfallband@gmail.com>
2017-11-25 18:35:11 +0900

    Add Form API to represent in-game instances of prototype (i.e. items)

---
+++ Assets/Alensia/Core/Common/Form.cs
@@ -0,0 +1,16 @@
+﻿using Alensia.Core.I18n;
+using UnityEngine;
+
+namespace Alensia.Core.Common
+{
+    public class Form : ScriptableObject, IForm
+    {
+        public string Name => _name;
+
+        public TranslatableText DisplayName => _displayName;
+
+        [SerializeField] private string _name;
+
+        [SerializeField] private TranslatableText _displayName;
+    }
+}
---
+++ Assets/Alensia/Core/Common/IFormInstance.cs
@@ -0,0 +1,7 @@
+﻿namespace Alensia.Core.Common
+{
+    public interface IFormInstance<out T> where T : IForm
+    {
+        T Form { get; }
+    }
+}
