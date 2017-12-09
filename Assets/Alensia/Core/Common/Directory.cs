da634f4759d2bd51132d1570c46d9e27b7be4f1e
Xavier Cho <mysticfallband@gmail.com>
2017-12-09 18:36:23 +0900

    Add POCO base class for IDirectory

---
+++ Assets/Alensia/Core/Common/Directory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Alensia.Core.Common
+{
+    public abstract class Directory<T> : IDirectory<T>, IEditorSettings
+        where T : class, INamed
+    {
+        protected abstract IEnumerable<T> Items { get; }
+
+        protected IDictionary<string, T> ItemMap
+        {
+            get
+            {
+                lock (this)
+                {
+                    if (_itemMap != null) return _itemMap;
+
+                    _itemMap = new Dictionary<string, T>();
+
+                    foreach (var item in Items)
+                    {
+                        _itemMap.Add(item.Name, item);
+                    }
+
+                    return _itemMap;
+                }
+            }
+        }
+
+        private IDictionary<string, T> _itemMap;
+
+        public bool Contains(string key) => ItemMap.ContainsKey(key);
+
+        public T this[string key] => ItemMap.ContainsKey(key) ? ItemMap[key] : null;
+    }
+}
