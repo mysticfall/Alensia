76620d5b015646a25099988ae9758b8850d59c9c
Xavier Cho <mysticfallband@gmail.com>
2017-12-10 15:13:06 +0900

    Add base class for exceptions with localized texts

---
+++ Assets/Alensia/Core/I18n/LocalizedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Alensia.Core.I18n
+{
+    public class LocalizedException : Exception, ITranslatable
+    {
+        public TranslatableText LocalizedMessage { get; }
+
+        public LocalizedException(
+            TranslatableText message, Exception innerException = null) : base(message?.Text, innerException)
+        {
+            LocalizedMessage = message;
+        }
+
+        public string Translate(ITranslator translator) => LocalizedMessage?.Translate(translator);
+    }
+}
