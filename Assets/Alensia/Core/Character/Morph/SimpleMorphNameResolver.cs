38a38afae648ad98de49ac291ffd19d1875f0c7f
Xavier Cho <mysticfallband@gmail.com>
2017-12-17 20:41:11 +0900

    Add simple IMorphNameResolver implementation

---
+++ Assets/Alensia/Core/Character/Morph/SimpleMorphNameResolver.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace Alensia.Core.Character.Morph
+{
+    public class SimpleMorphNameResolver : MonoBehaviour, IMorphNameResolver
+    {
+        public string Resolve(string text) => Capitalize(SplitCamelCase(text)).Trim();
+
+        private static string Capitalize(string text) =>
+            text.Length < 2 ? text : text[0].ToString().ToUpper() + text.Substring(1);
+
+        private static string SplitCamelCase(string text) =>
+            Regex.Replace(text, "([A-Z])", " $1", RegexOptions.Compiled);
+    }
+}
